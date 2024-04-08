3e31a35c54d214580259968a7c659a6d03307c2d
antonbashlovka <anton.bashlovka@gmail.com>
2024-04-08 18:22:51 +0500

    done

--- notes.py
+++ notes.py
@@ -10,23 +10,76 @@
 #последующим вводом данных, как-то ещё, на усмотрение студента.
 
 import csv
+import datetime
+
+def readMyCsv():
+    with open("notes.csv", encoding="utf-8") as file_reader:
+        notes = csv.reader(file_reader, delimiter = ";")
+        count = 0
+        arrNotes = []
+        for row in notes:
+            arrNotes.append(row)
+            count += 1
+
+    return arrNotes
+
+def writeMyCsv(notes):
+    with open("notes.csv", 'w', encoding="utf-8", newline='') as csv_file_writer:
+        writer = csv.writer(csv_file_writer, delimiter =";", quotechar='|')
+        for row in notes:
+            writer.writerow(row)
+    print('Запись сделана')
+
 
 command = input("Это программа для заметок, введите команду - all, new, red, del: ")
 
 if (command == "all"):
-    with open("notes.csv", encoding="utf-8") as file_reader:
-        notes = csv.reader(file_reader, delimiter = ";")
-        count = 0
+    notes = readMyCsv()
 
-        for row in notes:
-            # Вывод строк
-            print(f'    {row[0]} - {row[3]} {row[1]} : {row[2]} ')
-            count += 1
+    for row in notes:
+        print(f'{row[0]} : {row[1]} {row[2]} {row[3]} ' )
+
+    print(f'Количество записей в файле: {len(notes)}')
 
 
+if (command == "new"):
+    header = input("Введите заголовок: ")
+    text = input("Введите текст: ")
+    current_date_time = datetime.datetime.now()
+    current_date = current_date_time.date()
+    #current_time = current_date_time.time()
+    notes = readMyCsv()
 
+    notes.append([len(notes)+1, header, text, current_date])
 
-    print(f'Всего записей в файле: {count}')
+    writeMyCsv(notes)
+
+if (command == "red"):
+
+    notes = readMyCsv()
+    num = input("Введите номер записи для изменений: ")
+    tmp_notes = notes
+
+    for row in notes:
+        if (row[0] == num):
+                index = int (row[0])-1
+                header = input("Введите заголовок: ")
+                text = input("Введите текст: ")
+                current_date_time = datetime.datetime.now()
+                current_date = current_date_time.date()
+                tmp_notes[index] = [num, header, text, current_date]
+                writeMyCsv(tmp_notes)
+
+if (command == "del"):
+
+    notes = readMyCsv()
+    num = input("Введите номер записи для удаления: ")
+    tmp_notes = notes
+
+    for row in notes:
+        if (row[0] == num):
+            tmp_notes.remove(row)
+            writeMyCsv(tmp_notes)
 
 
 
